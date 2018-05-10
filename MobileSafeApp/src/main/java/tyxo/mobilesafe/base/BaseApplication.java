30e8e03bc6d8d6f3ed4bb43981901516f4c42150
tyxo842 <tyxo842@163.com>
2018-05-10 16:33:14 +0800

    解决activity not found; 添加aar,但是未解决bug(FuliActivity)

--- MobileSafeApp/src/main/java/tyxo/mobilesafe/base/BaseApplication.java
+++ MobileSafeApp/src/main/java/tyxo/mobilesafe/base/BaseApplication.java
@@ -1,42 +1,43 @@
 package tyxo.mobilesafe.base;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 /**
-* @author ly
-* @created at 2016/7/28 11:22
-* @des : 	将修改过后的.class文件打成jar包(正确代码运行之后生成的class文件).
- * */
-/**
- * 	动态改变BaseDexClassLoader对象间接引用的dexElements.
- * 	必须在应用启动的时候，将这个hack_dex.jar插入到dexElements，否则肯定会出事故的。
+ * @author ly
+ * @created at 2016/7/28 11:22
+ * @des : 	将修改过后的.class文件打成jar包(正确代码运行之后生成的class文件).
  */
-public class BaseApplication extends Application {
 
-	private static BaseApplication instance;
+/**
+ * 动态改变BaseDexClassLoader对象间接引用的dexElements.
+ * 必须在应用启动的时候，将这个hack_dex.jar插入到dexElements，否则肯定会出事故的。
+ */
+public class BaseApplication extends MultiDexApplication {
 
-	/**
-	 *	用法--->(先将改正后的class文件打成jar包,然后在类加载之前调用方法):
-	 *
-	 *	[---在自己目录 : E:\WorkPlace_Studio_Test\MobileSafe\hackdex\build\intermediates\classes\debug,运行(生成hack.jar):
-	 * 			jar cvf hack.jar dodola/hackdex/*
-	 *	在自己目录 : E:\StudioSDK\build-tools\23.0.3 , 运行dx命令 , 会在此路径的文件夹下生成 hack_dex.jar.
-	 	dx  --dex --output hack_dex.jar (此处将hack.jar 拖过来,直接打名字会找不到)---这个已经打包好,不用管.---]
+    private static BaseApplication instance;
 
-		只需打下面修改的包 :
-	 		下方两个命令,同上(在固定路径下,命令行运行):
-	 		jar cvf path.jar dodola/hotfix/BugClass.class
-	 		dx  --dex --output path_dex.jar path.jar
-		方法 :
-			File dexPath = new File(getDir("dex", Context.MODE_PRIVATE), "path_dex.jar");
-			Utils.prepareDex(this.getApplicationContext(), dexPath, "path_dex.jar");
-			//HotFix.patch(this, dexPath.getAbsolutePath(), "dodola.hotfix.BugClass");
-	 		HotFix.patch(this, dexPath.getAbsolutePath(), "tyxo.mobilesafe.activity.ImageViewerActivity");
-	 		ps : 修改 ImageViewerActivity ,上面两个 HotFix.patch 都可以成功!!! why ???
-	 */
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		instance = this;
+    /**
+     * 用法--->(先将改正后的class文件打成jar包,然后在类加载之前调用方法):
+     * <p>
+     * [---在自己目录 : E:\WorkPlace_Studio_Test\MobileSafe\hackdex\build\intermediates\classes\debug,运行(生成hack.jar):
+     * jar cvf hack.jar dodola/hackdex/*
+     * 在自己目录 : E:\StudioSDK\build-tools\23.0.3 , 运行dx命令 , 会在此路径的文件夹下生成 hack_dex.jar.
+     * dx  --dex --output hack_dex.jar (此处将hack.jar 拖过来,直接打名字会找不到)---这个已经打包好,不用管.---]
+     * <p>
+     * 只需打下面修改的包 :
+     * 下方两个命令,同上(在固定路径下,命令行运行):
+     * jar cvf path.jar dodola/hotfix/BugClass.class
+     * dx  --dex --output path_dex.jar path.jar
+     * 方法 :
+     * File dexPath = new File(getDir("dex", Context.MODE_PRIVATE), "path_dex.jar");
+     * Utils.prepareDex(this.getApplicationContext(), dexPath, "path_dex.jar");
+     * //HotFix.patch(this, dexPath.getAbsolutePath(), "dodola.hotfix.BugClass");
+     * HotFix.patch(this, dexPath.getAbsolutePath(), "tyxo.mobilesafe.activity.ImageViewerActivity");
+     * ps : 修改 ImageViewerActivity ,上面两个 HotFix.patch 都可以成功!!! why ???
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
 
 		/*// 读取path_dex.jar文件
 		File dexPath = new File(getDir("dex", Context.MODE_PRIVATE), "hackdex_dex.jar");
@@ -47,11 +48,11 @@ public class BaseApplication extends Application {
 		} catch (ClassNotFoundException e ) {
 			e.printStackTrace();
 		}*/
-	}
+    }
 
-	public static BaseApplication getInstance() {
-		return instance;
-	}
+    public static BaseApplication getInstance() {
+        return instance;
+    }
 }
 
 
